10ecdc23687623db8ece61e31237ce4560611d14
Tyler Kropp <kroppt@purdue.edu>
2018-02-16 04:46:58 -0500

    Optimize algorithm to increment square root

--- SquareRootsUpdated/Program.cs
+++ SquareRootsUpdated/Program.cs
@@ -11,41 +11,37 @@ namespace Rooter
             FindSquares(0, 999999999999999999); //Find all applicable numbers up to an abritrarily large number
         }
 
-        public static bool IsPerfectSquare(long num) //Determines if a number is a perfect square
+        public static bool IsPerfectSquare(ulong num) //Determines if a number is a perfect square
         {
             double squareRoot = Math.Sqrt(num);
             bool isSquare = squareRoot % 1 == 0;
             return isSquare;
         }
 
-        public static long IncreaseNums(long num) //Increases each digit in a number by 1
+        public static ulong IncreaseNums(ulong num) //Increases each digit in a number by 1
         {
             {
-                string numString = num.ToString();
-                string ones = "";
-                for (int i = 0; i < numString.Length; i++)
+                ulong strlen = (ulong) num.ToString().Length;
+                ulong ones = 0;
+                for (ulong i = 1, p = 1; i <= strlen; p *= 10, i++)
                 {
-                    ones += "1";
+                    ones += p;
                 }
-                return Convert.ToInt64(numString) + Convert.ToInt64(ones);
+                return num + ones;
             }
         }
 
-        public static void FindSquares(int x, long y) //Main code to find applicable numbers from x to y
+        public static void FindSquares(ulong x, ulong y) //Main code to find applicable numbers from x to y
         {
+            x = (ulong) Math.Sqrt((double) x);
+            y = (ulong) Math.Sqrt((double) y);
             Stopwatch watch = new Stopwatch();
             watch.Start();
-            for (int i = x; i < y; i++)
+            for (ulong i = x; i < y; i++)
             {
-                if (!((i.ToString()).Contains('9')) & i.ToString().EndsWith("25"))
+                if (IsPerfectSquare(IncreaseNums(i*i)))
                 {
-                    if (IsPerfectSquare(i))
-                    {
-                        if (IsPerfectSquare(IncreaseNums(i)))
-                        {
-                            Console.WriteLine(i + " " + watch.Elapsed);
-                        }
-                    }
+                    Console.WriteLine(i*i + " " + watch.Elapsed);
                 }
             }
 
